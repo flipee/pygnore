e64dceaab680a6ff9bd3be60a1a95441c7d09954
Filipe Nascimento <filipe.nascimento01@fatec.sp.gov.br>
2018-12-03 22:09:37 -0200

    improve api endpoints

--- pygnore/api.py
+++ pygnore/api.py
@@ -20,27 +20,20 @@ cache = TTLCache(maxsize=1, ttl=300)
 @cached(cache)
 def get_templates():
     request = urljoin(gitignore_api, "list")
-
     try:
         response = urlopen(request).read().decode()
     except (HTTPError, URLError, ValueError):
         raise
 
-    pre_formated_list = [i for i in response.split("\n")]
-
-    templates_list = []
-
-    for i in pre_formated_list:
-        templates_list.extend(i.split(","))
-
-    return templates_list
+    template_list = [y for i in response.split("\n") for y in i.split(",")]
+    return template_list
 
 
 def get_gitignore(templates):
-    templates_list = get_templates()
+    template_list = get_templates()
 
     for i in templates:
-        if i.lower() not in templates_list:
+        if i.lower() not in template_list:
             raise UnsupportedTemplateError(i)
 
     formated_templates = ",".join(templates)
